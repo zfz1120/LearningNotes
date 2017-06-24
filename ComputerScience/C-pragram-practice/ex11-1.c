e236eec0413288cadb7811485407c279c43f8793
libing403 <1540845930@qq.com>
2017-06-24 14:12:56 +0800

    c编程练习

--- ComputerScience/C-pragram-practice/ex11-1.c
+++ ComputerScience/C-pragram-practice/ex11-1.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
- 	int main()
-	{
-		int n=5;
-		char str[6];
-		char *pstr;
-		printf("请输入5个字符:\n");
-		pstr=strin(str,n);
-		printf("输入的字符为：\n %s\n",*pstr);
-	}
-	char * strin(char * str,int n)
-	{
-		int count=0;
-		char ch;
-        while(count<=n)
-		{
-			ch=getchar();
-			str[count]=strcat(str,ch);
-		}
-		return str;
-
-	}
+#include<string.h>
+#define LEN 5
+void strin(char * str);
+int main(void)
+{
+	char str[LEN];
+	printf("请输入5个字符：\n");
+	strin(str);
+	puts("输入的5个字符为：");
+	for(int n=0;n<LEN;n++)
+			putchar(str[n]);
+}
+void strin(char * str)
+{
+	for(int n=0;n<LEN;n++)
+			str[n]=getchar();
+}
