45ed12a74db190ca25b84af148c50627dfd63556
libing403 <1540845930@qq.com>
2018-01-05 23:50:52 +0800

    谐波运动程序

--- Robotics/Trajectory planning for Automatic Machines and Robots/ExamplesCode/src/mp_harmonic.c
+++ Robotics/Trajectory planning for Automatic Machines and Robots/ExamplesCode/src/mp_harmonic.c
@@ -6,31 +6,77 @@ author: LiBing
 #include<math.h>
 #include<mp_harmonic.h>
 
-//运动规划参数结构体
+#define MP_OK 0			/*速度规划成功*/
+#define PARA_ERR 1		/*输入参数非法*/
+
+
+#define MIN_ERR  1.0E-5 /*数值比较允许的误差*/
+//输入参数
 typedef struct {
+	double t0;
+	double t1;
 	double q0;
 	double q1;
+	double dT;
 	double vs;
 	double ve;
 	double acc;
 	double jerk;
 	double snap;
-	double dT;
-	double T;
+}input_para;
 
+//运动规划参数结构体
+typedef struct {
+	double t0;
+	double t1;
+	double T;
+	double q0;
+	double q1;
+	double dT;
+	double h;
+	double vs;
+	double ve;
+	double acc;
+	double jerk;
+	double snap;
 }mp_para;
 
 //轨迹规划函数接口
-mp_para inout;
-int mp_harmonic_traj(mp_para *st)
+
+int ret = 0;
+int mp_harmonic_traj(const input_para *in,mp_para *out)
 {
+	if ((ret = mp_para_initial(in, out)) != MP_OK)
+	{
+		return ret;
+	}
+
+
+
+
 
 }
 
-static int mp_para_initial(mp_para *st)
-{
-	inout.vs = st->vs;
-	inout.ve = st->ve;
-	inout.dT = st->dT;
+
+/*初始化输入参数*/
+static int mp_para_initial(const input_para *in, mp_para *out)
+{	
+	
+	if (in->t0 < MIN_ERR || in->t1 < MIN_ERR || in->ve<MIN_ERR
+		|| in->vs<MIN_ERR ||in->q0<MIN_ERR ||in->q1<MIN_ERR)
+	{
+		ret = PARA_ERR;
+		return ret;
+	}
+	out->t0 = in->t0;
+	out->t1 = in->t1;
+	out->T = in->t1 - in->t0;
+	out->q0 = in->q0;
+	out->q1 = in->q1;
+	out->h = in->q1 - in->q0;
+	out->vs = in->vs;
+	out->ve = in->ve;
+	out->dT = in->dT;
+	return MP_OK;
 
 }
--- Robotics/Trajectory planning for Automatic Machines and Robots/ExamplesCode/src/mp_harmonic.h
+++ Robotics/Trajectory planning for Automatic Machines and Robots/ExamplesCode/src/mp_harmonic.h
@@ -5,8 +5,10 @@
 #ifdef __cplusplus
 extern "C" {
 #endif // 
+extern struct input_para;
 extern struct mp_para;
-int mp_harmonic_traj(mp_para *inout);
+
+int mp_harmonic_traj(const input_para *input,mp_para *output);
 
 #ifdef __cplusplus
 }
